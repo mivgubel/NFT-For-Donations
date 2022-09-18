b2de6441d22ee0c735f07390c48861d66fe0c152
Erik Valle <erik.valle@gmail.com>
2022-09-18 14:48:32 -0500

    corrige logo

--- Frontend/src/Utils/Constants/Images.js
+++ Frontend/src/Utils/Constants/Images.js
@@ -1,7 +1,7 @@
 
 
 const ABOUT_IMAGE = "https://asepyme.com/wp-content/uploads/2017/07/como-constituir-una-fundacion-2-.jpg";
-const LOGO = "./favicon.ico";
+const LOGO = "./solidaritylogo.png";
 const causa1 = "./images/causa1.png"
 
 
