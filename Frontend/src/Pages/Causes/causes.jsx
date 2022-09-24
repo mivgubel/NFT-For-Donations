c8a830c3366c3ce166bb45d6b7d6e823ca896ec5
Guillermo De La Mora <guillermodevillagomez@gmail.com>
2022-09-24 02:23:14 -0500

    Cambio

--- Frontend/src/Pages/Causes/causes.jsx
+++ Frontend/src/Pages/Causes/causes.jsx
@@ -24,16 +24,25 @@ export default function Causas() {
     <div>
       {
         causes.map(cause => {
-          const {address, image, name, description} = cause;
+          const {address, image, name} = cause;
+          const description = ` Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                Donec vulputate quis lorem sed tristique. Vestibulum hendrerit vitae mi et tristique. Aliquam erat volutpat. 
+                                Nunc sollicitudin tincidunt ante in tincidunt. Vivamus eget ligula pellentesque lacus tristique sollicitudin. 
+                                In aliquam risus ac justo porta, at luctus mauris vestibulum. Duis condimentum mi vitae neque cursus placerat. 
+                                Nam nibh magna, tristique at est ac, lacinia volutpat dui. In sed enim eu nibh porttitor tincidunt vel ut libero. 
+                                Donec quis felis eu enim pellentesque gravida in et ligula. Morbi porta, velit eu ultrices porta, lacus augue fringilla 
+                                felis, id gravida risus leo vel urna. Proin dictum, augue quis laoreet mollis, elit mi viverra purus, 
+                                ac volutpat leo nisi ut mauris. Pellentesque finibus turpis non odio tincidunt, vel interdum eros posuere. 
+                                Fusce facilisis auctor ipsum, vel mattis nunc pulvinar eget. Morbi odio diam, vehicula vitae nisl eget, fermentum blandit leo. Phasellus pharetra libero a laoreet volutpat.`
           const {container, titleAndImage, imageStyle, descriptionStyle, info} = s;
           return (
             <div key={address} className={container}>
               <div className={titleAndImage}>
                 <p className="title">{name}</p>
-                {/* <img className={imageStyle} src={image} alt={name}/> */}
+                <img className={imageStyle} src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdBSvZZp18wtEy_J_K_yP-lpeoN8y7llHa2tynzXYRY1PrRGovQ8qJdzhY7ePvkEslCHg&usqp=CAU"} alt={name}/>
               </div>
               <div className={descriptionStyle}>
-                {/* <p >{description.slice(0,300) + '...'}</p> */}
+                <p >{description.slice(0,300) + '...'}</p>
                 <NavLink className={info} to={`${CAUSES}/${address}`}>More Info</NavLink>
               </div>
             </div>
