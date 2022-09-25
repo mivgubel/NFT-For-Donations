c8b53b80f68fd92302b93f2c5f4c8797f1c6e04d
Guillermo De La Mora <guillermodevillagomez@gmail.com>
2022-09-24 23:32:25 -0500

    Se agrega redireccion boton home

--- Frontend/src/Components/About/about.jsx
+++ Frontend/src/Components/About/about.jsx
@@ -9,6 +9,8 @@ import { ABOUT_IMAGE } from '../../Utils/Constants/Images';
 
 //Bootstrap
 import {Button, Col} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import { CAUSES } from '../../Utils/Constants/Routes';
 
 const About = () => {
   const {container, subContainer, description, aboutImage} = s;
@@ -24,7 +26,9 @@ const About = () => {
         <p className={description}>
 The project aims to help social causes and at the same time sponsor it with a monthly recurring income, derived from the return of the original donation.
 Promoting transparency through the use of blockchain.</p>
-        <Button className="generalButton lg"  variant="primary">Find a cause</Button>
+        <NavLink to={`${CAUSES}`}>
+          <Button className="generalButton lg"  variant="primary">Find a cause</Button>
+        </NavLink>
       </div>
       </Col>
       <Col>
