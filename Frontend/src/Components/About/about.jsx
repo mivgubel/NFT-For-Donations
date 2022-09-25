eb0b843c5e83fd431e34fd8a089978ea977db99c
Guillermo De La Mora <guillermodevillagomez@gmail.com>
2022-09-24 22:55:31 -0500

    Se edita redireccion de boton about

--- Frontend/src/Components/About/about.jsx
+++ Frontend/src/Components/About/about.jsx
@@ -1,5 +1,6 @@
 //React
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 //style
 import s from './about.module.css';
@@ -9,16 +10,19 @@ import { ABOUT_IMAGE } from '../../Utils/Constants/Images';
 
 //Bootstrap
 import Button from 'react-bootstrap/Button';
+import { CAUSES } from '../../Utils/Constants/Routes';
 
 const About = () => {
   const {container, subContainer, description, aboutImage} = s;
   return(   
     <div className={container}>
       <div className={subContainer}>
-        <h1 className="bigTitle">About the title</h1>
+        <h1 className="bigTitle">About Solidarity</h1>
         <p className={description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque itaque aperiam explicabo? Voluptas, dolores! Dolor hic veniam ad sit voluptatum voluptatibus quos est libero placeat! Expedita velit reiciendis voluptatibus ab?
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque itaque aperiam explicabo? Voluptas, dolores! Dolor hic veniam ad sit voluptatum voluptatibus quos est libero placeat! Expedita velit reiciendis voluptatibus ab?</p>
-        <Button className="generalButton" variant="primary">Call to action</Button>
+        <NavLink className="nav-link" to={CAUSES}>
+          <Button className="generalButton" variant="primary">Call to action</Button>
+        </NavLink>
       </div>
       <img className={aboutImage} src={ABOUT_IMAGE} alt="Bailarines" />
     </div>
